c0a3b88f0667c3c4a53153fa261fb116a7fd8d5d
Tatrite <florian.fuchs31700@gmail.com>
2024-09-10 11:14:39 +0200

    first committ

--- score_de_Scrabble.py
+++ score_de_Scrabble.py
@@ -1,4 +1,4 @@
-def recup_score_letre(score_str : str ):
+def recup_score_letre(score_str : str):
     score={}
     sc_lst=score_str.split(" ")
     for l in range (0,len(sc_lst)-1,2):
@@ -14,15 +14,24 @@ def calc_mot(mot,score : dict):
                  sc+=0
     return sc
 
+def tri(list_mots : list):
+    list_mots.sort(key=lambda a: a[1], reverse=True)
+    return list_mots
+def affiche_res(list_mots):
+     for mot in list_mots:
+          print(mot[0].replace("\n",""))
+
 def score_Scrabble(chemin : str):
     with open(chemin, 'r') as donne:
         lignes=donne.readlines()
         score=recup_score_letre(lignes[0])
-        mots_sc={}
         mots=[]
         for i in range(1,len(lignes)):
-            mots.append(lignes[i])
-            mots_sc[lignes[i]] = calc_mot(lignes[i],score))
-score_Scrabble("test.txt")
+            mots.append((lignes[i],calc_mot(lignes[i],score)))
+        affiche_res(tri(mots))
+
+
+
+score_Scrabble(input("entrer le chemins d'un fichier : "))
 
         
