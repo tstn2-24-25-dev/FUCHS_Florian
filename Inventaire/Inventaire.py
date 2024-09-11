bd08a245f5fc09b4744bfd8cd7209bbda5e623b1
Tatrite <florian.fuchs31700@gmail.com>
2024-09-11 09:36:38 +0200

    modification arboraissance

--- Inventaire/Inventaire.py
+++ Inventaire/Inventaire.py
@@ -1,16 +1,28 @@
 def recup_ocurence(lignes : str):
-    for i in lignes:
-         i.split(" ")
+   ocurence={}
+   for i in lignes:
+      i=i.split(" ")
+      if not i[0] in ocurence.keys():
+         ocurence[i[0]] = int(i[1])
+      else :
+         ocurence[i[0]] += int(i[1])
+   return ocurence
 
-        
-
-
+def max_occurence(ocurence : dict):
+   maxi=max(ocurence.values())
+   maxs=""
+   for keys in ocurence.keys():
+      if ocurence[keys] == maxi:
+         maxs+=f"{keys} {ocurence[keys]} "
+   return maxs
 
 def inventaire(chemin : str):
     with open(chemin, 'r') as donne:
         lignes=donne.readlines()
-        stock=lignes.pop(0)
+        lignes.pop(0)
         ocurence=recup_ocurence(lignes)
-        affiche_res(ocurence)
-
+        print(max_occurence(ocurence))
+inventaire("Inventaire\\test.txt")
+inventaire("Inventaire\\test2")
+inventaire("Inventaire\\test3")
         
