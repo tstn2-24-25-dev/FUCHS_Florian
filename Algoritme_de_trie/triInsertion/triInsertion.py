6c4328d70457de9db77264756b47750226c20e1a
Tatrite <florian.fuchs31700@gmail.com>
2024-09-24 11:12:48 +0200

    insert

--- Algoritme_de_trie/triInsertion/triInsertion.py
+++ Algoritme_de_trie/triInsertion/triInsertion.py
@@ -3,7 +3,6 @@ def triInsertion(tab:list)->list:
     Description : La fontion triInsertion(tab) tri la liste tab en 
     utilisant la methode de trie par Insertion :
     Le tri par insertion fonctionne comme un jeu de cartes : on insère chaque carte à sa place.
-    Voici les étapes successives du tri par insertion en utilisant des tableaux en Markdown avec de la mise en forme en gras et des couleurs pour illustrer les déplacements des éléments dans le tableau.
     Parametre : tab : list : IN/OUT
     Sorti : tab : list
     Precondition : tab doit être une list d'entier
@@ -14,8 +13,14 @@ def triInsertion(tab:list)->list:
         return []
     else :
         assert type(tab[0])==int,"tab doit être une list d'entier ou vide"
-    
-    
-    
-    
-    
+    for i in range(len(tab)-1):
+        if tab[i]>tab[i+1]:
+            ele=tab.pop(i+1)
+            for j in range(i,-1,-1):
+                if j==0 and ele<tab[j]:
+                    tab.insert(j,ele)
+                elif ele>tab[j-1] and ele<tab[j]:
+                    tab.insert(j,ele)
+    return(tab)
+
+print(triInsertion([4,8,2,10,1,9,7,6,3,5]))
