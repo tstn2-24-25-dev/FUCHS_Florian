5580bd1a60121eca097a0b4ecc3022eaec2ff1e9
Tatrite <florian.fuchs31700@gmail.com>
2024-09-24 10:37:40 +0200

    insert

--- Algoritme_de_trie/triInsertion/triInsertion.py
+++ Algoritme_de_trie/triInsertion/triInsertion.py
@@ -1,12 +1,21 @@
-def triBulles(tab:list)->list:
+def triInsertion(tab:list)->list:
     '''
-    Description : La fontion triBulles(tab) tri la liste tab en 
-    utilisant la methode de trie a bulles :
-    Tout élément doit être plus petit que celui qui le suit
-    Comparer chaque élément avec l’élément qui le suit, et si l’ordre n’est pas bon, permuter ces deux éléments
-    Recommencer jusqu’à n’avoir plus aucune permutation à faire
+    Description : La fontion triInsertion(tab) tri la liste tab en 
+    utilisant la methode de trie par Insertion :
+    Le tri par insertion fonctionne comme un jeu de cartes : on insère chaque carte à sa place.
+    Voici les étapes successives du tri par insertion en utilisant des tableaux en Markdown avec de la mise en forme en gras et des couleurs pour illustrer les déplacements des éléments dans le tableau.
     Parametre : tab : list : IN/OUT
     Sorti : tab : list
     Precondition : tab doit être une list d'entier
     Postcondition : tab est modifier
     '''
+    assert type(tab)==list ,"tab doit être une list"
+    if len(tab)==0:
+        return []
+    else :
+        assert type(tab[0])==int,"tab doit être une list d'entier ou vide"
+    
+    
+    
+    
+    
